b53c805c385d9ff153272c59508c106c0c3a5784
Vaishnavi Janardhan <35659656+vaishnavi-janardhan@users.noreply.github.com>
2018-04-04 07:14:06 +0530

    Few edits to improve the layout

--- app/src/main/java/com/example/vaishjanardhan/myclock/Fragment/AlarmFragment.java
+++ app/src/main/java/com/example/vaishjanardhan/myclock/Fragment/AlarmFragment.java
@@ -2,15 +2,18 @@ package com.example.vaishjanardhan.myclock.Fragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.SwitchCompat;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 
 import com.example.vaishjanardhan.myclock.R;
 
 //Class extending fragment
 public class AlarmFragment extends Fragment {
-
     //Overridden method onCreateView
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
